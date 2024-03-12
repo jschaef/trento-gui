413ddec4640e79476efc06e59456af470820c241
Jochen Schäfer <ajoschz@gmail.com>
2024-03-12 16:52:30 +0100

    minor layout change

--- pages/🔒logout.py
+++ pages/🔒logout.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
+col1, *_ = st.columns([0.2,0.8])
 st.session_state.pop("logged_in", None)
 st.session_state.pop("login_task", None)
 st.session_state.pop("_login_task", None)
 st.session_state.pop("user_role", None)
 st.session_state.pop("username", None)
-st.info("You have been logged out")
+col1.info("You have been logged out")
