8522cd248ab6013a70b1722dbf5f4f4ab5cc3465
Jochen Schäfer <jochen.schaefer@suse.com>
2025-01-23 14:31:32 +0100

    new py file for the admin container menu

---
+++ code/helpers/container_service.py
@@ -0,0 +1,16 @@
+import helpers.printer_help as p_help
+from streamlit.delta_generator import DeltaGenerator
+
+def trento_service():
+    pass
+
+def update_trento():
+    pass
+
+def uninstall_trento():
+    pass
+
+def show_containers():
+    pass
+
+def delete_containers():
